5d38fce05799f770c5e05bb88999dd7b6540de8f
kkayacan <kkayacan@gmail.com>
2020-03-17 16:10:27 +0000

    Build config

--- public_html/controller/App.controller.js
+++ public_html/controller/App.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	'sap/ui/core/Fragment'
+], function (Controller, Fragment) {
 	"use strict";
 
 	return Controller.extend("com.keremkayacan.base.controller.App", {
@@ -11,6 +12,34 @@ sap.ui.define([
 
 		onAfterRendering: function () {
 			sap.ui.core.BusyIndicator.hide();
+		},
+
+		onExit: function () {
+			if (this._oPopover) {
+				this._oPopover.destroy();
+			}
+		},
+
+		onInfoPress: function (oEvent) {
+			var oButton = oEvent.getSource();
+
+			if (!this._oPopover) {
+				Fragment.load({
+					name: "com.keremkayacan.base.view.infoPopover",
+					controller: this
+				}).then(function (oPopover) {
+					this._oPopover = oPopover;
+					this.getView().addDependent(this._oPopover);
+					this._oPopover.bindElement("/ProductCollection/0");
+					this._oPopover.openBy(oButton);
+				}.bind(this));
+			} else {
+				this._oPopover.openBy(oButton);
+			}
+		},
+
+		handleCloseButton: function (oEvent) {
+			this._oPopover.close();
 		}
 
 	});
